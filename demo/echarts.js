a43a4b9e4d96c07abbcdffc2803b3a7d1cc9dd11
Gang Tao <gtao@splunk.com>
2019-11-17 22:06:16 -0800

    fix charactor set for demo page

--- demo/echarts.js
+++ demo/echarts.js
@@ -5,7 +5,7 @@ function drawBarChart() {
     const option = {
         xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun中午']
         },
         yAxis: {
             type: 'value'
--- demo/g2.js
+++ demo/g2.js
@@ -1,27 +1,27 @@
 function drawBarChart() {
     var data = [{
-        year: '1951',
+        year: '1951 年',
         sales: 38
     }, {
-        year: '1952',
+        year: '1952 年',
         sales: 52
     }, {
-        year: '1956',
+        year: '1956 年',
         sales: 61
     }, {
-        year: '1957',
+        year: '1957 年',
         sales: 145
     }, {
-        year: '1958',
+        year: '1958 年',
         sales: 48
     }, {
-        year: '1959',
+        year: '1959 年',
         sales: 38
     }, {
-        year: '1960',
+        year: '1960 年',
         sales: 38
     }, {
-        year: '1962',
+        year: '1962 年',
         sales: 38
     }];
     var chart = new G2.Chart({
